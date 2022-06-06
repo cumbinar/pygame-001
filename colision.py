e5cb147de02cba03aea72c890776353c0b3be11d
cumbinar <cumbiweb@gmail.com>
2022-06-06 06:34:40 -0500

    posición aleatoria de cuadrado colisionado

--- colision.py
+++ colision.py
@@ -9,7 +9,7 @@ pygame.init()
 
 
 
-pygame.display.set_caption("CUMBI Mover cuadrado con el teclado")
+pygame.display.set_caption("CUMBI Perseguir un cuadrado con el teclado")
 
 size = 800, 500
 gris = 230, 230, 230
@@ -18,11 +18,10 @@ naranja = 255, 155, 0
 cuadrado_pos = [400, 400]
 cuadrado2_pos =[100,100]
 cuadrado_medida = [70, 70]
-paso = 20
+paso = 50
 
 COLOR_FUENTE = (6, 47, 253)
 screen = pygame.display.set_mode(size)
-
 
 def dibujar_texto(screen, texto, pos):
     fuente = pygame.font.SysFont('Barber Street_PersonalUseOnly', 20)
@@ -51,8 +50,11 @@ while run:
 
     screen.fill(gris)
     dibujar_texto(screen, "Mueva el cuadrado ROJO con las teclas direccionales", [220, 30])
-    pygame.draw.rect(screen, (naranja), (cuadrado2_pos[0], cuadrado2_pos[1], cuadrado_medida[0], cuadrado_medida[1]))
-    pygame.draw.rect(screen, (rojo),(cuadrado_pos[0], cuadrado_pos[1],cuadrado_medida[0], cuadrado_medida[1]))
+    cuadro2= pygame.draw.rect(screen, (naranja), (cuadrado2_pos[0], cuadrado2_pos[1], cuadrado_medida[0], cuadrado_medida[1]))
+    cuadro1 = pygame.draw.rect(screen, (rojo),(cuadrado_pos[0], cuadrado_pos[1],cuadrado_medida[0], cuadrado_medida[1]))
+    if cuadro1.colliderect(cuadro2): #detecta colision entre los dos cuadrados
+        cuadrado2_pos[0] = randint(100, 700)
+        cuadrado2_pos[1] = randint(100,400)
 
     
     pygame.display.update()
