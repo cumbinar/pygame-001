29b130fed076e176d92e2fd476cc7a58d5391ca4
cumbinar <cumbiweb@gmail.com>
2022-06-08 19:08:29 -0500

    cuadrado rojo aumenta el área con cada colisión

--- fondo-y-colision.py
+++ fondo-y-colision.py
@@ -12,13 +12,16 @@ screen = pygame.display.set_mode((ancho, alto))
 fps = 120  # variable para mostrar cuadros por segundo
 reloj = pygame.time.Clock()  # variable para el reloj
 # Cambio el título de la ventana
-pygame.display.set_caption("CUMBI Fondo en movimiento")
+pygame.display.set_caption("CUMBI Fondo en movimiento y colisióm")
 # Inicializamos variables
 rojo = 255, 0, 0
 naranja = 255, 155, 0
 cuadrado_pos = [400, 400]
 cuadrado2_pos = [100, 100]
 cuadrado_medida = [70, 70]
+cuadrado_rojo_medida = [40, 40]
+
+color_fuente = (6, 47, 253)
 paso = 50
 # Crea fondo
 # carga y optimiza imagen
@@ -26,6 +29,13 @@ fondo = pygame.image.load("imagenes/fondo-1.jpg").convert()
 
 x = 0  # variable para la coordenada X
 # Comenzamos el bucle del juego
+
+
+def dibujar_texto(screen, texto, pos):
+    fuente = pygame.font.SysFont('Barber Street_PersonalUseOnly', 20)
+    text = fuente.render(texto, 1, color_fuente)
+    screen.blit(text, pos)
+
 run = True
 while run:
 
@@ -52,17 +62,27 @@ while run:
     if x_relativa < ancho:
         screen.blit(fondo, (x_relativa, 0))
     x -= 1
-    #pygame.display.flip()
+    
     reloj.tick(fps)  # llama al reloj y pasa los cuadros por minuto
 # Salgo de pygame
+    dibujar_texto(screen, "Mueva el cuadrado ROJO con las teclas direccionales", [220, 25])
+    if (cuadrado_rojo_medida[0] or cuadrado_rojo_medida[1]) == 200:
+        dibujar_texto(screen, "Fin del Juego", [330, 150])
+        cuadrado_rojo_medida[0] = 50
+        cuadrado_rojo_medida[1] = 50
+    
 
-    cuadro2 = pygame.draw.rect(
-        screen, (naranja), (cuadrado2_pos[0], cuadrado2_pos[1], cuadrado_medida[0], cuadrado_medida[1]))
+    cuadro2 = pygame.draw.rect(screen, (naranja), (cuadrado2_pos[0], cuadrado2_pos[1], cuadrado_medida[0], cuadrado_medida[1]))
     cuadro1 = pygame.draw.rect(
-        screen, (rojo), (cuadrado_pos[0], cuadrado_pos[1], cuadrado_medida[0], cuadrado_medida[1]))
+        screen, (rojo), (cuadrado_pos[0], cuadrado_pos[1], cuadrado_rojo_medida[0], cuadrado_rojo_medida[1]))
     if cuadro1.colliderect(cuadro2):  # detecta colision entre los dos cuadrados
+        cuadrado_rojo_medida[0] += 5
+        cuadrado_rojo_medida[1] += 5
+        print(cuadrado_rojo_medida[0])
+       
         pygame.mixer.music.load('imagenes/clap.wav')
         pygame.mixer.music.play()
+   
         cuadrado2_pos[0] = randint(100, 700)
         cuadrado2_pos[1] = randint(100, 400)
    
